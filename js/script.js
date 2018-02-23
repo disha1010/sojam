e379863387943ab4bd162c69b41c226a31502e5b
disha1010 <diana.saginbaeva@gmail.com>
2018-02-23 09:15:08 +0300

    fix bugs

--- js/script.js
+++ js/script.js
@@ -4,15 +4,14 @@ $(document).ready(function () {
     $('.accordion-toggle').removeClass('active');
     $('.faq-answer').slideUp(300).removeClass('open');
   }
-  $('.accordion-toggle').click(function (e) {
+  $('.article-title-wrapper').click(function (e) {
     e.preventDefault();
-    if ($(this).is('.active')) {
+    if ($(this).find('.accordion-toggle').is('.active')) {
       close_accordion();
     } else {
       close_accordion();
-      $(this).addClass('active');
-      $(this)
-        .parents('.faq-item')
+      $(this).find('.accordion-toggle').addClass('active');
+      $(this).parents('.faq-item')
         .find('.faq-answer')
         .slideDown(300)
         .addClass('open');
@@ -21,7 +20,9 @@ $(document).ready(function () {
   // top-menu
   $('.navbar-toggle').on('click', function () {
     $(this).find('.nav-icon').toggleClass('open');
-    $('.navbar-collapse-sojam').animate({ width: 'toggle' });
+    $('.navbar-collapse-sojam').animate({
+      width: 'toggle'
+    });
   });
 
   // filter-nav
@@ -34,26 +35,35 @@ $(document).ready(function () {
     startAngle: -1.55,
     size: 26,
     value: 1,
-    animation: { duration: 4000, easing: "linear" },
-    fill: { color: '#fff' }
+    animation: {
+      duration: 4000,
+      easing: "linear"
+    },
+    fill: {
+      color: '#fff'
+    }
   }
   var progressBarOptionsFast = {
     startAngle: -1.55,
     size: 26,
     value: 1,
-    animation: { duration: 600, easing: "circleProgressEasing" },
-    fill: { color: '#fff' }
+    animation: {
+      duration: 600,
+      easing: "circleProgressEasing"
+    },
+    fill: {
+      color: '#fff'
+    }
   }
   var myIndicator = $('.main .carousel-control-sojam.right');
   myIndicator.circleProgress(progressBarOptionsSlow);
   // carousel
-  var myCarousel = $('#myCarousel');
+  var myCarousel = $('.main #myCarousel');
   myCarousel.on('slide.bs.carousel', function () {
     myIndicator.circleProgress(progressBarOptionsFast);
   });
   myCarousel.on('slid.bs.carousel', function () {
     myIndicator.circleProgress(progressBarOptionsSlow);
   });
-  
-});
 
+});
