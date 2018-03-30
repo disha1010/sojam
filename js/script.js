faa83527326772b114e551e10047cb30c3acf416
disha1010 <diana.saginbaeva@gmail.com>
2018-03-30 16:04:25 +0300

    fix bugs with preview image on feedback page

--- js/script.js
+++ js/script.js
@@ -198,15 +198,9 @@ $(document).ready(function () {
   // feedback image download
   function readURL(input) {
     if (input.files && input.files[0]) {
-      var imageHolder = $('.image-holder');
-      imageHolder.empty();
-
       var reader = new FileReader();
       reader.onload = function (e) {
-        $("<img />", {
-          "src": e.target.result,
-          "class": "thumb-image"
-        }).appendTo(imageHolder);
+        $('.image-holder').css('background-image', 'url('+ e.target.result +')');
       }
       reader.readAsDataURL(input.files[0]);
     }
